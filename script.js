8ec5c80f64fcd73035c7a5bbca780ceb1b6f0788
Clirim Sadiku <clirim100@gmail.com>
2022-11-30 17:34:54 +0100

    fixed the input text

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addBtn = document.querySelector('#addbtn');
 const tasks = document.querySelector('#tasks');
 const text = document.querySelector('#text');
-const mainText = document.querySelector('#mainText');
+const mainText = document.querySelector('#maintext');
 
 
 const createList = () => {
@@ -9,7 +9,7 @@ const createList = () => {
     tasksContainer.classList.add('col','d-flex','justify-content-center',"mt-3");
     tasksContainer.setAttribute('id','tasksContainer');
     tasks.appendChild(tasksContainer);
-
+    
     const content = document.createElement('div');
     tasksContainer.appendChild(content);
     
@@ -18,17 +18,19 @@ const createList = () => {
     input1.setAttribute('id', 'text');
     input1.setAttribute('type','text');
     input1.setAttribute('readonly','readonly');
+    input1.value = mainText.value;
     
     content.appendChild(input1);
     
     
     const actions = document.createElement('div');
+    tasksContainer.appendChild(actions);
     
     const btnEdit = document.createElement('button');
     btnEdit.classList.add("bg-dark", "text-info" ,"rounded-pill", "ms-2","me-2");
     btnEdit.setAttribute('id', 'edit');
     btnEdit.innerHTML= 'Edit';
-    
+    actions.appendChild(btnEdit);
     
     const btnDelete = document.createElement('button');
     btnDelete.classList.add("bg-dark", "text-danger" ,"rounded-pill");
@@ -36,16 +38,15 @@ const createList = () => {
     btnDelete.innerHTML= 'Delete';
     btnDelete.addEventListener('click', () => tasksContainer.remove());
     
+    actions.appendChild(btnDelete);
     
-    tasksContainer.appendChild(actions);
     
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type','checkbox');
     checkbox.classList.add('ms-2');
     
-    actions.appendChild(btnEdit);
-    actions.appendChild(btnDelete);
     actions.appendChild(checkbox);
 };
+// John was here
 
 addBtn.addEventListener('click', createList);
